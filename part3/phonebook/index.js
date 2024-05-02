6ca7e679b58cb0d018e173fa10cd7fa94fb353db
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2024-05-02 18:16:34 +0200

    3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   {
@@ -37,7 +40,6 @@ app.get("/api/persons", (req, res) => {
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   const person = persons.find((person) => person.id === id);
-  console.log(person);
   if (person) {
     res.json(person);
   } else {
