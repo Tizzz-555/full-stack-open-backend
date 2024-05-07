f5d013c141dcc23235758970677b566d489b9a0b
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2024-05-07 19:35:57 +0200

    3.12

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -14,18 +14,31 @@ mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+  name: String,
+  number: String,
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const Person = mongoose.model("Person", personSchema);
 
-const note = new Note({
-  content: "HTML is easy",
-  important: true,
-});
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
 
-note.save().then((result) => {
-  console.log("note saved!");
-  mongoose.connection.close();
-});
+  person.save().then((result) => {
+    console.log(result);
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  console.log("both name and number are required");
+  process.exit(1);
+}
