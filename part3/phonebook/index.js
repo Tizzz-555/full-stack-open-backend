ae18495e8b3a5bcaec48325f139c3dfb936083e8
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2024-05-12 16:23:25 +0200

    3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
 require("dotenv").config();
+
 const Person = require("./models/person");
+
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
@@ -46,36 +48,6 @@ app.get("/info", (req, res) => {
   `);
 });
 
-// const generateRandomId = () => {
-//   const randomId = Math.floor(Math.random() * 10000);
-//   return randomId;
-// };
-
-// app.post("/api/persons", (req, res) => {
-//   const body = req.body;
-
-//   if (!body.name || !body.number) {
-//     return res.status(400).json({
-//       error: !body.name ? "Name missing" : "Number missing",
-//     });
-//   }
-
-//   if (persons.map((person) => person.name).includes(body.name)) {
-//     return res.status(400).json({
-//       error: "Name must be unique ",
-//     });
-//   }
-
-//   const person = {
-//     name: body.name,
-//     number: body.number,
-//     id: generateRandomId(),
-//   };
-
-//   persons = persons.concat(person);
-//   res.json(person);
-// });
-
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
@@ -91,23 +63,31 @@ app.post("/api/persons", (req, res) => {
   });
 
   person.save().then((savedNote) => {
-    response.json(savedNote);
+    res.json(savedNote);
   });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (!person) {
-    return res.status(404).json({ error: "Person not found" });
-  }
-
-  persons = persons.filter((person) => person.id !== id);
-
-  res.status(200).json(person);
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      console.log(result);
+      if (result) {
+        res
+          .status(200)
+          .send({
+            name: `${result.name}`,
+            success: `${result.name} successfully deleted`,
+          });
+      } else {
+        res.status(400).json({ error: "No entry with this Id" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send({ error: "malformatted id" });
+    });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
