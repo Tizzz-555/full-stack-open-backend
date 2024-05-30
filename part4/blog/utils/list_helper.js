98de0c137738a23998c11d396f4bad1edc06772a
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2024-05-30 21:18:26 +0200

    4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,22 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogposts) => {
+  // const allLikes = [];
+  // blogposts.forEach((b) => allLikes.push(b));
+
+  // console.log(allLikes);
+
+  // const reducer = (sum, item) => {
+  //   return sum + item;
+  // };
+
+  // return allLikes.reduce(reducer, 0);
+  let sum = 0;
+  blogposts.forEach((b) => (sum += b.likes));
+  return sum;
+};
 module.exports = {
   dummy,
+  totalLikes,
 };
