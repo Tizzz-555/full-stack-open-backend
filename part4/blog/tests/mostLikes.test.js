5e2105b5129cb1a15cd83e478991fb18681566a3
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2024-06-04 18:43:39 +0200

    refactor and start 4.7

--- part4/blog/tests/mostLikes.test.js
+++ part4/blog/tests/mostLikes.test.js
@@ -7,28 +7,28 @@ const {
   blogs,
 } = require("../data/blogsData");
 
-describe("most blogs", () => {
+describe("most likes", () => {
   const testResult = {
     author: "Edsger W. Dijkstra",
-    blogs: 1,
+    likes: 5,
   };
 
-  test("when list has only one blog, return the author and count 1 blog", () => {
-    const result = listHelper.mostBlogs(listWithOneBlog);
+  test("when list has only one blog, return the author and the blog's likes", () => {
+    const result = listHelper.mostLikes(listWithOneBlog);
     assert.deepStrictEqual(result, testResult);
   });
 
-  test("when list has many blogs, return the author who wrote the most and how many he wrote", () => {
-    const result = listHelper.mostBlogs(blogs);
+  test("when list has many blogs, return the author who got the most likes and how many they got", () => {
+    const result = listHelper.mostLikes(blogs);
     const testResult = {
-      author: "Robert C. Martin",
-      blogs: 3,
+      author: "Edsger W. Dijkstra",
+      likes: 17,
     };
     assert.deepStrictEqual(result, testResult);
   });
 
   test("when list has no blogs, return an error message", () => {
-    const result = listHelper.mostBlogs(listWithNoBlogs);
+    const result = listHelper.mostLikes(listWithNoBlogs);
     assert.strictEqual(result, "There are no blog posts");
   });
 });
