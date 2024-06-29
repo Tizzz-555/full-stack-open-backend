89bd6c8540b2d94b900138d85c6fe25770080467
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2024-06-29 14:07:43 +0200

    4.17

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,29 +1,29 @@
+const User = require("../models/user");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
 blogsRouter.get("/", async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   res.json(blogs);
 });
 
 blogsRouter.post("/", async (req, res) => {
-  const body = new Blog(req.body);
+  const body = req.body;
+
+  const user = await User.findById(body.userId);
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user.id,
   });
 
-  // if (!blog.title || !blog.url) {
-  //   res.status(400).end();
-  // } else {
-  //   const savedBlog = await blog.save();
-  //   res.status(201).json(savedBlog);
-  // }
-
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
   res.status(201).json(savedBlog);
 });
 
