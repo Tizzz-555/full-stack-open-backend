e1164897e8970be3994d09541db97f5a90c54825
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2025-01-30 19:50:54 +0100

    8.7

--- part8/library/library-backend.js
+++ part8/library/library-backend.js
@@ -111,6 +111,11 @@ const typeDefs = `
     author: String!
     genres: [String!] 
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int
+    ): Author
   }
 `;
 
@@ -167,6 +172,16 @@ const resolvers = {
 
       books = books.concat(book);
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a));
+      return updatedAuthor;
     },
   },
 };
